34dcf83a47d3cf5f0e7788daac6d0e7ec03706bd
John Wood <woodjohn185@gmail.com>
2021-03-03 11:19:12 -0700

    adding prod mode check

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import Amplify from 'aws-amplify';
 import aws_exports from './aws-exports';
+import { environment } from './environments/environment';
+import { enableProdMode } from '@angular/core';
+
+if (environment.production) {
+  enableProdMode();
+}
 
 Amplify.configure(aws_exports);
 
