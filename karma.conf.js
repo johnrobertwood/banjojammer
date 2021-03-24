a61e4ac11b4d27e807765f3c2f9370d394eeded9
John Wood <woodjohn185@gmail.com>
2021-03-24 10:16:22 -0600

    changing name to grapplingreview

--- karma.conf.js
+++ karma.conf.js
@@ -19,7 +19,7 @@ module.exports = function (config) {
       suppressAll: true, // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require("path").join(__dirname, "./coverage/jitspro"),
+      dir: require("path").join(__dirname, "./coverage/grapplingreview"),
       subdir: ".",
       reporters: [{ type: "html" }, { type: "text-summary" }],
     },
