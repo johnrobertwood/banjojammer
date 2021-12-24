cc07b12bf4939da7f0bf7365985c755febc96255
John Wood <jwood@Johns-MacBook-Pro.local>
2021-12-23 20:36:20 -0700

    configed on new machine

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "grapplejits009df1ec": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "AppClientSecret": "string"
+        }
+    }
+}
