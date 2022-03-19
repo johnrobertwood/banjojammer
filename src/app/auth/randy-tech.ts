8dbd4a74a1def207325a59164ffaa8182acdd691
John Wood <woodjohn185@gmail.com>
2022-03-18 22:42:54 -0600

    adding randy-tech details

--- src/app/auth/randy-tech.ts
+++ src/app/auth/randy-tech.ts
@@ -1,10 +1,10 @@
 const randyTech = {
-  kneeSlice: {
+  activeShooterResponseProtocol: {
     id: 0,
-    name: 'kneeSlice',
-    displayName: 'Knee Slice',
+    name: 'activeShooterResponseProtocol',
+    displayName: 'Active Shooter Response Protocol',
     prevTechnique: null,
-    nextTechnique: 'barDarce',
+    nextTechnique: 'defendingYourHomeGroundJanuary2018',
     quiz: {
       complete: false,
       question: 'How do you execute a knee slice from closed guard?',
@@ -45,15 +45,16 @@ const randyTech = {
     ],
     video: {
       url: 'https://player.vimeo.com/video/257845940',
-      thumbnail: 'https://img.youtube.com/vi/GmvNttCRsx0/mqdefault.jpg',
+      thumbnail:
+        'https://i.vimeocdn.com/video/685868180-c879e3d1287ff83185fcd6a061ee6c83719a91c3764020bcce5b2aa3c0950d7c-d_640',
     },
   },
-  barDarce: {
-    id: 1,
-    name: 'barDarce',
-    displayName: 'Bar Darce',
-    prevTechnique: 'kneeSlice',
-    nextTechnique: 'sevenYearOldChoke',
+  defendingYourHomeGroundJanuary2018: {
+    id: 10,
+    name: 'defendingYourHomeGroundJanuary2018',
+    displayName: 'Defending Your Home Ground January 2018 ',
+    prevTechnique: 'activeShooterResponseProtocol',
+    nextTechnique: 'strongHandPistol',
     quiz: {
       complete: false,
       question: 'What is the bar darce analogy?',
@@ -92,15 +93,64 @@ const randyTech = {
     ],
     video: {
       url: 'https://player.vimeo.com/video/253383057',
-      thumbnail: 'https://img.youtube.com/vi/t6FiiticH3Y/mqdefault.jpg',
+      thumbnail:
+        'https://i.vimeocdn.com/video/680255616-4b557188e8e15954172c1113d08e813e7af67b569233aaa9727f69c4dd7cc17f-d_640',
     },
   },
-  sevenYearOldChoke: {
-    id: 2,
-    name: 'sevenYearOldChoke',
-    displayName: 'Seven Year Old Choke',
-    prevTechnique: 'barDarce',
-    nextTechnique: 'monoplata',
+  strongHandPistol: {
+    id: 15,
+    name: 'strongHandPistol',
+    displayName: 'Strong Hand Pistol',
+    prevTechnique: 'defendingYourHomeGroundJanuary2018',
+    nextTechnique: 'movingToContact',
+    quiz: {
+      complete: false,
+      question: 'What is the bar darce analogy?',
+      responses: [
+        {
+          text: 'If you are at a bar and your buddy sitting next to you reaches for your drink it exposes their neck and arm',
+          correct: true,
+        },
+        {
+          text: 'Use your forearm as a bar for leverage on the darce',
+          correct: false,
+        },
+        {
+          text: 'The bar darce is best done from front headlock',
+          correct: false,
+        },
+        {
+          text: 'A bar darce is similar to a knee bar but its a choke',
+          correct: false,
+        },
+      ],
+    },
+    flashcard: {
+      complete: false,
+      question: 'What is the bar darce analogy?',
+      answer:
+        'An arm drag with a hop step to the side when opponent has you in front headlock',
+    },
+    notes: [
+      { text: 'Works best when side to side' },
+      { text: 'Partner is reaching across exposing arm and neck' },
+      { text: 'Close side is a darce, far side is anaconda' },
+      {
+        text: 'Short path from side up and under to darce, dont have to reach far',
+      },
+    ],
+    video: {
+      url: 'https://player.vimeo.com/video/226399320',
+      thumbnail:
+        'https://i.vimeocdn.com/video/645999868-1526be02fe2dfebd3b4ed163b223150064c84f38ae8471723dcfca91eefe559b-d_640',
+    },
+  },
+  movingToContact: {
+    id: 20,
+    name: 'movingToContact',
+    displayName: 'Moving To Contact',
+    prevTechnique: 'strongHandPistol',
+    nextTechnique: 'activeShooterInTheWorkplace',
     quiz: {
       complete: false,
       question: 'How do you do the seven year old choke?',
@@ -137,16 +187,17 @@ const randyTech = {
       { text: 'Pry the fingers as a last resort escape' },
     ],
     video: {
-      url: 'https://player.vimeo.com/video/226399320',
-      thumbnail: 'https://img.youtube.com/vi/4csIWClv-vo/mqdefault.jpg',
+      url: 'https://player.vimeo.com/video/205332429',
+      thumbnail:
+        'https://i.vimeocdn.com/video/620070868-b572dd3149f5bbc64201210579dce7d6b00baaf480da03d4f12539e8ff6e8415-d_640',
     },
   },
-  monoplata: {
-    id: 3,
-    name: 'monoplata',
-    displayName: 'Monoplata',
-    prevTechnique: 'sevenYearOldChoke',
-    nextTechnique: 'suckerDrag',
+  activeShooterInTheWorkplace: {
+    id: 30,
+    name: 'activeShooterInTheWorkplace',
+    displayName: 'Active Shooter In The Workplace',
+    prevTechnique: 'movingToContact',
+    nextTechnique: 'defensivePistolLevelFour',
     quiz: {
       complete: false,
       question: 'True or false: Monoplata is the _______ version of Omoplata?',
@@ -187,16 +238,17 @@ const randyTech = {
       },
     ],
     video: {
-      url: 'https://player.vimeo.com/video/205332429',
-      thumbnail: 'https://img.youtube.com/vi/XKBigNF7TME/mqdefault.jpg',
+      url: 'https://player.vimeo.com/video/201800635',
+      thumbnail:
+        'https://i.vimeocdn.com/video/615661803-194e6595fdd4de2606cf682bc39e86a2e7588d536088d25c4b38e361eb5e41ae-d_640',
     },
   },
-  suckerDrag: {
-    id: 4,
-    name: 'suckerDrag',
-    displayName: 'Sucker Drag',
-    prevTechnique: 'monoplata',
-    nextTechnique: 'triangleFromKnees',
+  defensivePistolLevelFour: {
+    id: 40,
+    name: 'defensivePistolLevelFour',
+    displayName: 'Defensive Pistol Level Four',
+    prevTechnique: 'activeShooterInTheWorkplace',
+    nextTechnique: 'defendingYourHomeGround',
     quiz: {
       complete: false,
       question: 'What is the sucker drag?',
@@ -231,16 +283,17 @@ const randyTech = {
       { text: 'Works from standing or from the knees' },
     ],
     video: {
-      url: 'https://player.vimeo.com/video/201800635',
-      thumbnail: 'https://img.youtube.com/vi/UC7vDdyXpe4/mqdefault.jpg',
+      url: 'https://player.vimeo.com/video/184632648',
+      thumbnail:
+        'https://i.vimeocdn.com/video/634534779-f27d6e622865cc0b34eae3b0f71ec62cefd07cd5df6fd20022868e852ff87a2f-d_640',
     },
   },
-  triangleFromKnees: {
-    id: 5,
-    name: 'triangleFromKnees',
-    displayName: 'Triangle From Knees',
-    prevTechnique: 'suckerDrag',
-    nextTechnique: 'darceOrGuillotineTrap',
+  defendingYourHomeGround: {
+    id: 50,
+    name: 'defendingYourHomeGround',
+    displayName: 'Defending Your Home Ground',
+    prevTechnique: 'defensivePistolLevelFour',
+    nextTechnique: 'defensivePistolLevelThree',
     quiz: {
       complete: false,
       question:
@@ -274,16 +327,17 @@ const randyTech = {
       },
     ],
     video: {
-      url: 'https://player.vimeo.com/video/184632648',
-      thumbnail: 'https://img.youtube.com/vi/1J3bZu_zKIE/mqdefault.jpg',
+      url: 'https://player.vimeo.com/video/182810796',
+      thumbnail:
+        'https://i.vimeocdn.com/video/634533140-155e926a554afb04ed352f78ba516b1b67a45f4191c1814de8ff6a3bf6ede544-d_640',
     },
   },
-  darceOrGuillotineTrap: {
-    id: 6,
-    name: 'darceOrGuillotineTrap',
-    displayName: 'Darce or Guillotine Trap',
-    prevTechnique: 'triangleFromKnees',
-    nextTechnique: 'bottomSideToSingleLeg',
+  defensivePistolLevelThree: {
+    id: 60,
+    name: 'defensivePistolLevelThree',
+    displayName: 'Defensive Pistol Level Three',
+    prevTechnique: 'defendingYourHomeGround',
+    nextTechnique: 'defensiveCarbineLevelTwo',
     quiz: {
       complete: false,
       question: 'What is the setup for Darce or Guillotine Trap?',
@@ -324,16 +378,17 @@ const randyTech = {
       },
     ],
     video: {
-      url: 'https://player.vimeo.com/video/182810796',
-      thumbnail: 'https://img.youtube.com/vi/Q5j5iwut2eQ/mqdefault.jpg',
+      url: 'https://player.vimeo.com/video/177502454',
+      thumbnail:
+        'https://i.vimeocdn.com/video/584934587-af277bd6210f119e74edb871d1671d556e534cc1aeae901bbdaeb9e8ba447aa4-d_640',
     },
   },
-  bottomSideToSingleLeg: {
-    id: 7,
-    name: 'bottomSideToSingleLeg',
-    displayName: 'Bottom Side To Single Leg',
-    prevTechnique: 'darceOrGuillotineTrap',
-    nextTechnique: 'crossAshiSweep',
+  defensiveCarbineLevelTwo: {
+    id: 70,
+    name: 'defensiveCarbineLevelTwo',
+    displayName: 'Defensive Carbine Level Two',
+    prevTechnique: 'defensivePistolLevelThree',
+    nextTechnique: 'defensivePistolLevelTwo',
     quiz: {
       complete: false,
       question:
@@ -372,16 +427,17 @@ const randyTech = {
       },
     ],
     video: {
-      url: 'https://player.vimeo.com/video/177502454',
-      thumbnail: 'https://img.youtube.com/vi/ySpx7x7dZbE/mqdefault.jpg',
+      url: 'https://player.vimeo.com/video/171513490',
+      thumbnail:
+        'https://i.vimeocdn.com/video/634534492-ed6d44c5c3377b502bd0e5ec44cbc5f904007b61f296b7e7e7df4c8188e737bd-d_640',
     },
   },
-  crossAshiSweep: {
-    id: 8,
-    name: 'crossAshiSweep',
-    displayName: 'Cross Ashi Sweep',
-    prevTechnique: 'bottomSideToSingleLeg',
-    nextTechnique: 'reverseArmbar',
+  defensivePistolLevelTwo: {
+    id: 80,
+    name: 'defensivePistolLevelTwo',
+    displayName: 'Defensive Pistol Level Two',
+    prevTechnique: 'defensiveCarbineLevelTwo',
+    nextTechnique: 'defensivePistolLevelOne',
     quiz: {
       complete: false,
       question: 'What are the steps for the cross ashi sweep?',
@@ -422,16 +478,17 @@ const randyTech = {
       { text: 'Throw in a submission after the pass like a darce' },
     ],
     video: {
-      url: 'https://player.vimeo.com/video/171513490',
-      thumbnail: 'https://img.youtube.com/vi/zXUKhU7TC-w/mqdefault.jpg',
+      url: 'https://player.vimeo.com/video/167912420',
+      thumbnail:
+        'https://i.vimeocdn.com/video/634534609-e60ece5b908173d4d55a3cd9db94262110ecef41489c4c5f5daeee04b7d4804f-d_640',
     },
   },
-  reverseArmbar: {
-    id: 9,
-    name: 'reverseArmbar',
-    displayName: 'Reverse Armbar',
-    prevTechnique: 'crossAshiSweep',
-    nextTechnique: 'paperCutterChoke',
+  defensivePistolLevelOne: {
+    id: 90,
+    name: 'defensivePistolLevelOne',
+    displayName: 'Defensive Pistol Level One',
+    prevTechnique: 'defensivePistolLevelTwo',
+    nextTechnique: 'pppMarch2016',
     quiz: {
       complete: false,
       question: 'What are the steps for a reverse armbar?',
@@ -470,8 +527,205 @@ const randyTech = {
       { text: 'Practice the cross arm elbow to elbow grip for the finish' },
     ],
     video: {
-      url: 'https://player.vimeo.com/video/167912420',
-      thumbnail: 'https://img.youtube.com/vi/GpBqyOjidSU/mqdefault.jpg',
+      url: 'https://player.vimeo.com/video/160052156',
+      thumbnail:
+        'https://i.vimeocdn.com/video/561942597-30ee500707c04f18657578efd2da29ee029bb59179cf8cb18ce2588c105602ac-d_640',
+    },
+  },
+  pppMarch2016: {
+    id: 100,
+    name: 'pppMarch2016',
+    displayName: 'Practical Pistol Proficiency March 2016',
+    prevTechnique: 'defensivePistolLevelOne',
+    nextTechnique: 'precisionRifleMarksmanship',
+    quiz: {
+      complete: false,
+      question: 'What are the steps for a reverse armbar?',
+      responses: [
+        {
+          text: 'Shoot in, scoot low for underhooks, pull them up onto butterfly then trap the arm',
+          correct: true,
+        },
+        {
+          text: 'Americana',
+          correct: false,
+        },
+        {
+          text: 'Heel hook',
+          correct: false,
+        },
+        {
+          text: 'Triangle',
+          correct: false,
+        },
+      ],
+    },
+    flashcard: {
+      complete: false,
+      question: 'What are the steps for a reverse armbar?',
+      answer:
+        'Scoot in low to butterfly with underhooks, pull them back and force their hands onto mat then trap the arm',
+    },
+    notes: [
+      {
+        text: 'Scoot in and get a body lock, then lean back to get them to post',
+      },
+      {
+        text: 'Scoot back then use head, shoulder and face to clamp his arm straight',
+      },
+      { text: 'Practice the cross arm elbow to elbow grip for the finish' },
+    ],
+    video: {
+      url: 'https://player.vimeo.com/video/159129026',
+      thumbnail:
+        'https://i.vimeocdn.com/video/560797551-6ff2fe4135d56ee33e3453bc60004a09f32c0c83bafd835737c73d3eb9011e26-d_640',
+    },
+  },
+  precisionRifleMarksmanship: {
+    id: 110,
+    name: 'precisionRifleMarksmanship',
+    displayName: 'Precision Rifle Marksmanship',
+    prevTechnique: 'pppMarch2016',
+    nextTechnique: 'couplesSafetySeminar',
+    quiz: {
+      complete: false,
+      question: 'What are the steps for a reverse armbar?',
+      responses: [
+        {
+          text: 'Shoot in, scoot low for underhooks, pull them up onto butterfly then trap the arm',
+          correct: true,
+        },
+        {
+          text: 'Americana',
+          correct: false,
+        },
+        {
+          text: 'Heel hook',
+          correct: false,
+        },
+        {
+          text: 'Triangle',
+          correct: false,
+        },
+      ],
+    },
+    flashcard: {
+      complete: false,
+      question: 'What are the steps for a reverse armbar?',
+      answer:
+        'Scoot in low to butterfly with underhooks, pull them back and force their hands onto mat then trap the arm',
+    },
+    notes: [
+      {
+        text: 'Scoot in and get a body lock, then lean back to get them to post',
+      },
+      {
+        text: 'Scoot back then use head, shoulder and face to clamp his arm straight',
+      },
+      { text: 'Practice the cross arm elbow to elbow grip for the finish' },
+    ],
+    video: {
+      url: 'https://player.vimeo.com/video/158097373',
+      thumbnail:
+        'https://i.vimeocdn.com/video/559513837-28c3f48fdec79c64f809f5b0b0b7aa36a160efa773d48505af44c2df5db0cde2-d_640',
+    },
+  },
+  couplesSafetySeminar: {
+    id: 120,
+    name: 'couplesSafetySeminar',
+    displayName: 'Couples Safety Seminar',
+    prevTechnique: 'precisionRifleMarksmanship',
+    nextTechnique: 'pppClassJanuary2016',
+    quiz: {
+      complete: false,
+      question: 'What are the steps for a reverse armbar?',
+      responses: [
+        {
+          text: 'Shoot in, scoot low for underhooks, pull them up onto butterfly then trap the arm',
+          correct: true,
+        },
+        {
+          text: 'Americana',
+          correct: false,
+        },
+        {
+          text: 'Heel hook',
+          correct: false,
+        },
+        {
+          text: 'Triangle',
+          correct: false,
+        },
+      ],
+    },
+    flashcard: {
+      complete: false,
+      question: 'What are the steps for a reverse armbar?',
+      answer:
+        'Scoot in low to butterfly with underhooks, pull them back and force their hands onto mat then trap the arm',
+    },
+    notes: [
+      {
+        text: 'Scoot in and get a body lock, then lean back to get them to post',
+      },
+      {
+        text: 'Scoot back then use head, shoulder and face to clamp his arm straight',
+      },
+      { text: 'Practice the cross arm elbow to elbow grip for the finish' },
+    ],
+    video: {
+      url: 'https://player.vimeo.com/video/157687186',
+      thumbnail:
+        'https://i.vimeocdn.com/video/634531343-be8bf1d487063f16f5330eec2a2a9890591a15f4800e4cb82ccc2f2df27a015c-d_640',
+    },
+  },
+  pppClassJanuary2016: {
+    id: 130,
+    name: 'pppClassJanuary2016',
+    displayName: 'Practical Pistol Proficiency January 2016',
+    prevTechnique: 'couplesSafetySeminar',
+    nextTechnique: null,
+    quiz: {
+      complete: false,
+      question: 'What are the steps for a reverse armbar?',
+      responses: [
+        {
+          text: 'Shoot in, scoot low for underhooks, pull them up onto butterfly then trap the arm',
+          correct: true,
+        },
+        {
+          text: 'Americana',
+          correct: false,
+        },
+        {
+          text: 'Heel hook',
+          correct: false,
+        },
+        {
+          text: 'Triangle',
+          correct: false,
+        },
+      ],
+    },
+    flashcard: {
+      complete: false,
+      question: 'What are the steps for a reverse armbar?',
+      answer:
+        'Scoot in low to butterfly with underhooks, pull them back and force their hands onto mat then trap the arm',
+    },
+    notes: [
+      {
+        text: 'Scoot in and get a body lock, then lean back to get them to post',
+      },
+      {
+        text: 'Scoot back then use head, shoulder and face to clamp his arm straight',
+      },
+      { text: 'Practice the cross arm elbow to elbow grip for the finish' },
+    ],
+    video: {
+      url: 'https://player.vimeo.com/video/153177543',
+      thumbnail:
+        'https://i.vimeocdn.com/video/634534680-a0ec71a5ea33bada24fae6a9b14648fe7eff7a19cb296db9d04705376555e4f6-d_640',
     },
   },
 };
