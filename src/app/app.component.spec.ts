93e5ccd215b9b29f0b5f7a74582f7e767e03974a
John Wood <woodjohn185@gmail.com>
2022-03-18 23:25:17 -0600

    switching to warrior creed brand

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -154,8 +154,8 @@ describe('AppComponent', () => {
       expect(component).toBeDefined();
     });
 
-    it('should contain "Glover Jitsu"', () => {
-      expect(h1.textContent).toContain('Glover Jitsu');
+    it('should contain "Warrior Creed"', () => {
+      expect(h1.textContent).toContain('Warrior Creed');
     });
 
     it('selectedName is undefined before OnInit', () => {
