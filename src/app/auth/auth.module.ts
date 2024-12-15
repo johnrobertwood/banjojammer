98e296ab5a7519d7179047b05d2caad96de4e16f
John Wood <johnwood@Mac-mini.attlocal.net>
2024-12-15 16:06:03 -0600

    removing more material legacy

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
 import { AuthRoutingModule } from './auth-routing.module';
 
 import { ConfirmRegistrationComponent } from './confirm-registration/confirm-registration.component';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, AuthRoutingModule, MatButtonModule],
+  imports: [CommonModule, FormsModule, AuthRoutingModule],
   declarations: [ConfirmRegistrationComponent],
 })
 export class AuthenticationModule {}
